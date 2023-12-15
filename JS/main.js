d8a666da9a6fa5102a3b6fe5410196b36531a188
Amira-Abdelmoeen <amiraamur300@gmail.com>
2023-12-15 16:46:05 +0200

    Edit Content Quotes

--- JS/main.js
+++ JS/main.js
@@ -4,8 +4,8 @@ var quotes =[
         writer: "Oscar Wilde"
     },
     {
-        quote: "I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.",
-        writer: "Marilyn Monroe"
+        quote: "To live is the rarest thing in the world. Most people exist, that is all.",
+        writer: "Oscar Wilde"
     },
     {
         quote: "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.",
